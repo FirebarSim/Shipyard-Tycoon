cffacb0498659f8dca057180849989dd653fe315
FirebarSim <32907623+FirebarSim@users.noreply.github.com>
2018-11-16 18:21:37 +0000

    Commit for Travel
    Commit before changing computers.

---
+++ Shipyard Tycoon/Assets/Controllers/WorldController.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class WorldController : MonoBehaviour {
+
+    public Sprite landSprite;
+    public Sprite deepSprite;
+    public Sprite shallowSprite;
+    public Sprite dryingSprite;
+
+    World world;
+
+	// Use this for initialization
+	void Start () {
+        world = new World();
+        //world.RandomiseTiles();
+        world.ReadMap();
+
+        //Create GameObject for each tile to show them
+        for (int x = 0; x< world.Width; x++)
+        {
+            for (int y = 0; y < world.Height; y++)
+            {
+                Tile tile_data = world.GetTileAt(x, y);
+
+                GameObject tile_go = new GameObject();
+                tile_go.name = "Tile_" + x + "_" + y;
+                tile_go.transform.position = new Vector3(tile_data.X, tile_data.Y, 0);
+
+                SpriteRenderer tile_sr = tile_go.AddComponent<SpriteRenderer>();
+                if(tile_data.Type == Tile.TileType.Land)
+                {
+                    tile_sr.sprite = landSprite;
+                }
+                else if(tile_data.Type == Tile.TileType.DeepWater)
+                {
+                    tile_sr.sprite = deepSprite;
+                }
+                else if (tile_data.Type == Tile.TileType.ShallowWater)
+                {
+                    tile_sr.sprite = shallowSprite;
+                }
+                else if (tile_data.Type == Tile.TileType.Drying)
+                {
+                    tile_sr.sprite = dryingSprite;
+                }
+
+                tile_data.RegisterTileTypeChangedCallback( (tile) => { OnTileTypeChanged(tile, tile_go); } );
+            }
+        }
+
+	}
+
+    float randomiseTileTimer = 2f;
+    	
+	// Update is called once per frame
+	void Update () {
+        randomiseTileTimer -= Time.deltaTime;
+
+        if(randomiseTileTimer < 0)
+        {
+            world.RandomiseTiles();
+            randomiseTileTimer = 2f;
+        }
+	}
+
+    void OnTileTypeChanged (Tile tile_data, GameObject tile_go)
+    {
+        if (tile_data.Type == Tile.TileType.Land)
+        {
+            tile_go.GetComponent<SpriteRenderer>().sprite = landSprite;
+        }
+        else if (tile_data.Type == Tile.TileType.DeepWater)
+        {
+            tile_go.GetComponent<SpriteRenderer>().sprite = deepSprite;
+        }
+        else if (tile_data.Type == Tile.TileType.ShallowWater)
+        {
+            tile_go.GetComponent<SpriteRenderer>().sprite = shallowSprite;
+        }
+        else if (tile_data.Type == Tile.TileType.Drying)
+        {
+            tile_go.GetComponent<SpriteRenderer>().sprite = dryingSprite;
+        }
+        else
+        {
+            Debug.LogError("Unrecognised Tile Type");
+        }
+    }
+}
---
+++ Shipyard Tycoon/Assets/DataModel/World.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class World
+{
+    Tile[,] tiles;
+    int width;
+    public int Width
+    {
+        get
+        {
+            return width;
+        }
+    }
+    int height;
+    public int Height
+    {
+        get
+        {
+            return height;
+        }
+    }
+
+    public World (int width = 100, int height = 100)
+    {
+        this.width = width;
+        this.height = height;
+
+        tiles = new Tile[width, height];
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                tiles[x,y] = new Tile(this, x, y);
+            }
+        }
+
+        Debug.Log("World created with " + (width * height) + " tiles.");
+
+    }
+
+    public void RandomiseTiles ()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                if (Random.Range(0,2) == 0)
+                {
+                    tiles[x, y].Type = Tile.TileType.DeepWater;
+                }
+                else
+                {
+                    tiles[x, y].Type = Tile.TileType.Land;
+                }
+            }
+        }
+    }
+
+    public void ReadMap ()
+    {
+        int line_index = 0;
+        int column_index;
+        string[] lines = System.IO.File.ReadAllLines(@"D:\GitHub\Shipyard Tycoon\Shipyard Tycoon\Assets\Maps\Map.txt");
+        foreach (string line in lines)
+        {
+            column_index = 0;
+            string[] values = line.Split(' ');
+            foreach (string value in values)
+            {
+                if (value.ToString() == "0")
+                {
+                    tiles[column_index, line_index].Type = Tile.TileType.DeepWater;
+                }
+                else if (value.ToString() == "1")
+                {
+                    tiles[column_index, line_index].Type = Tile.TileType.ShallowWater;
+                }
+                else if (value.ToString() == "2")
+                {
+                    tiles[column_index, line_index].Type = Tile.TileType.Drying;
+                }
+                else
+                {
+                    tiles[column_index, line_index].Type = Tile.TileType.Land;
+                }
+                column_index++;
+            }
+            line_index++;
+        }
+    }
+
+    public Tile GetTileAt (int x, int y)
+    {
+        if (x>width ||x < 0)
+        {
+            Debug.LogError("Tile (" + x + ", " + y + ") is out of range.");
+            return null;
+        }
+        return tiles[x, y];
+    }
+}
